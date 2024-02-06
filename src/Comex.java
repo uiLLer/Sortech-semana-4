1e20f5cd19328a6b41ad15fccc5472ce2ca1bf8a
miller <mmillermonteiro@gmail.com>
2024-02-06 20:05:23 -0300

    Commited 4 questions

---
+++ src/Comex.java
@@ -0,0 +1,5 @@
+public class Comex {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Seja bem vindo ao COMEX!");
+    }
+}
---
+++ src/ListagemProdutos.java
@@ -0,0 +1,15 @@
+public class ListagemProdutos {
+    public static void main(String[] args) {
+        String li = "Livro";
+        String ce = "Celular";
+        float precoLi = 49.95f;
+        float precoCe = 1470.76f;
+        System.out.println(">>> Listagem de Produtos");
+        System.out.println("_".repeat(50));
+        System.out.println("Nome: " + li);
+        System.out.println("Data de Nascimento: " + precoLi);
+        System.out.println("_".repeat(50));
+        System.out.println("Nome: " + ce);
+        System.out.println("Data de Nascimento: " + precoCe);
+    }
+}
